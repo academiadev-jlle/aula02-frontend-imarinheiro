7073755003f62e7eeac186a8178db1b2169077be
Iago Marinheiro Elias <go.marinheiro@gmail.com>
2018-10-27 23:50:10 -0300

    add: exercicio object

---
+++ exercicio-object/exercicio-1.js
@@ -0,0 +1,13 @@
+// 1 -
+// Escreva uma função que possa ser utilizada para fazer a comparação de dois
+// objetos pelos valores de suas propriedades
+// Exemplo de retorno esperado:
+// let obj1 = { ‘nome’: ‘Pedro’ };
+// let obj2 = { ‘nome’: ‘Pedro’ };
+// console.log(obj1 === obj2); // false!
+// console.log(deepEqual(obj1, obj2)); // true!
+// Considere que o seu objeto pode ter outros objetos como propriedade :)
+
+let compareObj = (object1, object2) => {
+
+};
