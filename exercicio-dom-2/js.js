77a122e18a7c10c0a1d33ac04beefd984d2ff5ff
Iago Marinheiro Elias <go.marinheiro@gmail.com>
2018-10-27 23:49:30 -0300

    add: exercicio function

---
+++ exercicio-dom-2/js.js
@@ -0,0 +1,8 @@
+// 2)
+// Utilize a mesma estrutura criada no exercício anterior, porém neste exercício crie
+// uma tabela com dados vindos de uma API Rest qualquer. O formulário pode servir
+// para selecionar alguma entidade a ser carregada ou criar um filtro na API.
+// Pode ser por exemplo: https://pokeapi.co, ou https://swapi.co/
+// Obs: para resolução deste exercício, aprenda a utilizar o objeto XMLHttpRequest para
+// criar uma requisição HTTP GET para a API escolhida.
+// Melhore a legibilidade do seu código utilizando Promises.
